f5b068c3c4080b80b1e3d81273aa016a9101b98a
Shawn Carere <shawncarere@gmail.com>
2019-12-20 10:18:37 -0500

    began work on fft function

--- shawn/functions.py
+++ shawn/functions.py
@@ -1,9 +1,120 @@
 import pyqtgraph as pg
 import numpy as np
 from PyQt5 import QtGui
-from pylsl import StreamInlet,resolve_byprop
+from pylsl import StreamInlet, StreamOutlet, StreamInfo, resolve_byprop
+import threading
 
-def plotTimeDomain(stream_info, chunkwidth, fs=0, channels=0, timewin=50, tickfactor=5, size=(1500, 800), title=None):
+def fft_backend(input_stream, output_stream, window_length=256, pow2=True, window_type=np.hamming):
+    
+    #################################
+    ## Stream Inlet and Outlet Creation
+    #################################
+
+    #streams = resolve_byprop("name",input_stream.name(),timeout= 10)
+    #input_stream = streams[0]
+    print(input_stream)
+    print(input_stream.name())
+    inlet = StreamInlet(input_stream, max_chunklen=12, recover=True)
+    inlet.open_stream() # Stream is opened implicitely on first call of pull chunk, but opening now for clarity
+
+    # Create StreamOutlet to push data to output stream
+    outlet = StreamOutlet(output_stream)
+
+    ###################################
+    ## FFT
+    ###################################
+    
+    buffer = np.empty((0,5))
+    window = window_type(window_length)
+    while(True):
+        input_chunk = inlet.pull_chunk() # Pull Chunk
+        print(np.shape(input_chunk))
+
+        if input_chunk[0]: # Check for available chunk
+            buffer = np.append(buffer, input_chunk[0], axis=0)
+
+            if (len(buffer) >= window_length):
+                # Take data from buffer
+                data = buffer[0:window_length]
+                data = np.transpose(data)
+
+                # Get frequency labels/bins
+                freq_labels = np.fft.rfftfreq(window_length, 1/input_stream.nominal_srate())
+
+                # Take FFT of data for each channel
+                data_windowed = []
+                data_fft = []
+                psd = []
+                for i in range(0, output_stream.channel_count()):
+                    # Multiply data by window
+                    data_windowed.append(data[i] - np.mean(data[i], axis=0))
+                    data_windowed[i] = data_windowed[i] * window
+
+                    # Get FFT
+                    data_fft.append(np.fft.rfft(data_windowed[i], n=window_length, axis=0))
+                    data_fft[i] = data_fft[i]/window_length
+
+                    # Convert FFT to PSD
+                    psd.append(abs(data_fft[i])) # Take absolute value
+                    # Assume input signal is real-valued and double power to account for negative frequencies 
+                    # DC power (psd[i][0]) only occurs once and does not need to be doubled)
+                    psd[i][1:] = 2*psd[i][1:]
+
+                # Create Output Data Packet in shape 2 x N (Where N is the # of discrete frequencies)
+                # The first dimension of output sample contains the data of shape CHANNELS x N
+                # The second dimension contains the N labels for the frequencies in Hz 
+                psd = np.transpose(psd)
+                psd = psd.tolist()
+                freq_labels = freq_labels.tolist()
+                output_sample = (psd, freq_labels)
+                print(np.shape(output_sample))
+
+                # Push fft transform for each channel using outlet
+                #outlet.push_chunk(output_sample)
+
+
+
+def fft(input_stream, output_stream_name='default', window_length=256, pow2=True, window_type=np.hamming, channels=0):
+
+    #################################
+    ## Create New Output StreamInfo Objectcd 
+    #################################
+
+    # Set Default Output Stream Name
+    if (output_stream_name == 'default'):
+        output_stream_name = str(input_stream.name() + '-PSD')
+
+    # Get number of channels to transform
+    if(channels == 0):
+        channels = input_stream.channel_count() # Get number of channels
+
+    # Create Output StreamInfo Object
+    output_stream = StreamInfo(name=output_stream_name, 
+        type='PSD', 
+        channel_count=channels, 
+        nominal_srate=input_stream.nominal_srate(),
+        channel_format='float32',
+        source_id=input_stream.source_id())
+
+    ####################################
+    ## Create Thread to Run fft_backend
+    ####################################
+    #fft_backend(input_stream, output_stream)
+
+    # Currently if you run function in a diff thread it does not work
+    thread = threading.Thread(target=fft_backend, 
+        kwargs=dict(input_stream=input_stream, 
+            output_stream=output_stream, 
+            window_length=window_length, 
+            pow2=pow2,
+            window_type=window_type))
+
+    thread.start()
+
+    return output_stream
+
+
+def plotTimeDomain(stream_info, chunkwidth=0, fs=0, channels=0, timewin=50, tickfactor=5, size=(1500, 800), title=None):
     """Plot Real-Time domain in the time domain using a scrolling plot.
 
     Accepts a pylsl StreamInlet Object and plots chunks in real-time as they are recieved
@@ -53,7 +164,7 @@ def plotTimeDomain(stream_info, chunkwidth, fs=0, channels=0, timewin=50, tickfa
     ## Initialize Constants
     XWIN = timewin*fs # Width of X-Axis in samples
     XTICKS = (int)((timewin + 1)/tickfactor) # Number of labels to have on X-Axis
-    CHUNKPERIOD = chunkwidth*(1/fs) # The length of each chunk in seconds
+    #CHUNKPERIOD = chunkwidth*(1/fs) # The length of each chunk in seconds
 
     ##################################
     ## Figure and Plot Set Up
@@ -106,9 +217,12 @@ def plotTimeDomain(stream_info, chunkwidth, fs=0, channels=0, timewin=50, tickfa
     while(True):
         chunk = inlet.pull_chunk()
 
-        if np.shape(chunk) == (2, chunkwidth): # Check for available chunk
+        # (something is wierd with dummy chunks, get chunks of diff sizes, data comes in too fast)
+        if chunk: # Check for available chunk 
+            print(np.shape(chunk))
             chunkdata = np.transpose(chunk[0]) # Get chunk data and transpose to be CHANNELS x CHUNKLENTH
-
+            chunkperiod = len(chunkdata[0])*(1/fs)
+            xticks = [x - chunkperiod for x in xticks] # Update location of x-labels
 
             # Update x-axis locations and labels
             if(xticks[0] < 0): # Check if a label has crossed to the negative side of the y-axis
@@ -145,12 +259,8 @@ def plotTimeDomain(stream_info, chunkwidth, fs=0, channels=0, timewin=50, tickfa
     
     return True
 
-import pyqtgraph as pg
-import numpy as np
-from PyQt5 import QtGui
-from pylsl import StreamInlet
 
-def plotFreqDomain(stream_info, chunkwidth, fs=0, channels=0, size=(1500, 1500), title=None):
+def plotFreqDomain(stream_info, chunkwidth, channels=0, size=(1500, 1500), title=None):
     """Plot Real-Time in the frequency domain using a static x-axis and changing y axis values.
 
     Accepts a pylsl StreamInlet Object and plots chunks in real-time as they are recieved
@@ -177,10 +287,6 @@ def plotFreqDomain(stream_info, chunkwidth, fs=0, channels=0, size=(1500, 1500),
     #################################
     ## Variable Initialization
     #################################
-
-    ## Get/Check Default Params
-    if(fs == 0):
-        fs = stream_info.nominal_srate() # Get sampling rate
 
     if(channels == 0):
         channels = stream_info.channel_count() # Get number of channels
--- shawn/testPylsl.py
+++ shawn/testPylsl.py
@@ -4,7 +4,7 @@ import pylsl
 import time
 import numpy as np
 import time
-from functions import plotTimeDomain, plotFreqDomain
+from functions import plotTimeDomain, plotFreqDomain, fft
 
 ########################
 ## Create a stream
@@ -20,27 +20,9 @@ stream = streams[0]
 #stream_info = getStream_info(dummy_streamer)
 #sam trying his new code with your thing
 
-print("have the steam info object")
 
-# Create stream inlet to accept stream object data
-plotFreqDomain(stream, 12, title='EEG Data')
+fft(stream)
+#plotTimeDomain(stream, 12, title='EEG Data')
 
-print("past")
-#print('\nChannels: %d' % stream.channel_count())
-#print('Time: %f', time.time())
 
-#print('\nEEG Sample: ')
-# Sometimes pull_sample times out, also sometimes it returns different sized arrays for the samples. Not consistently getting a sample for all channels
-# while(1):
-#     x = streamIn.pull_chunk()
-#     if all(x):
-#         print(x)
-#     #if not np.shape(x) == (2, 0):
-#         print(np.shape(x))
-#         #print(time.time())
-#         #print(time.time() + streamIn.time_correction())
-
-# Delete objects and stop stream
-#streamIn.__del__() # Throws weird errors
-#EEGstream.__del__() # Also throws weird errors
 dummy_streamer.stop()
